6782032c980f3437211c01965831d04866bad0dc
Víctor García <gc.victor@gmail.com>
2023-11-23 15:15:29 +0100

    feat: update api path

--- src/http/http.constants.ts
+++ src/http/http.constants.ts
@@ -3,5 +3,5 @@ export const QUERY_STUDIO_ALLOWED_ORIGIN: string | undefined = process.env.QUERY
 export const QUERY_STUDIO_CLIENT_URL = process.env.QUERY_STUDIO_CLIENT_URL;
 export const QUERY_STUDIO_QUERY_SERVER = process.env.QUERY_STUDIO_QUERY_SERVER;
 // Query API
-export const QUERY_API_USER_TOKEN_VALUE = "/user/token/value";
-export const QUERY_API_QUERY = "/query";
+export const QUERY_API_USER_TOKEN_VALUE = "/_/user/token/value";
+export const QUERY_API_QUERY = "/_/query";
